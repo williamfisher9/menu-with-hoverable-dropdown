274900a7251ec030c6aeeff25a9312a7dbb70d08
williamfisher9 <98424965+williamfisher9@users.noreply.github.com>
2024-07-18 01:40:52 -0500

    responsive-design

---
+++ script.js
@@ -0,0 +1,72 @@
+const collapsibleMenu = document.querySelector(".collapsible-menu");
+const menuBars = document.querySelector(".menu-bars");
+const servicesIcon = document.querySelector(".services-icon");
+const applicationsIcon = document.querySelector(".applications-icon");
+
+const servicesCollapsibleMenu = document.querySelector(".services-collapsible-menu");
+const applicationsCollapsibleMenu = document.querySelector(".applications-collapsible-menu");
+
+// hide collapsa
+window.addEventListener("click", (event) => {
+    if(collapsibleMenu.style.display == "block" && event.target.tagName == "BODY"){
+        collapsibleMenu.style.display="none";
+        applicationsCollapsibleMenu.style.display="none";
+        servicesCollapsibleMenu.style.display="none";
+        menuDisplayed = false;
+        serviceMenuDisplayed = false;
+        applicationsMenuDisplayed = false;
+    }
+})
+
+window.addEventListener("resize", () => {
+    console.log(window.innerWidth);
+
+    if(window.innerWidth > 975){
+        collapsibleMenu.style.display="none";
+        applicationsCollapsibleMenu.style.display="none";
+        servicesCollapsibleMenu.style.display="none";
+        menuDisplayed = false;
+        serviceMenuDisplayed = false;
+        applicationsMenuDisplayed = false;
+    }
+})
+
+let menuDisplayed = false;
+
+menuBars.addEventListener("click", () => {
+    if(!menuDisplayed) {
+        collapsibleMenu.style.display="block";
+        menuDisplayed = true;
+    } else {
+        collapsibleMenu.style.display="none";
+        servicesCollapsibleMenu.style.display="none";
+        applicationsCollapsibleMenu.style.display="none";
+        menuDisplayed = false;
+        serviceMenuDisplayed = false;
+        applicationsMenuDisplayed = false;
+    }
+})
+
+let serviceMenuDisplayed = false;
+
+servicesIcon.addEventListener("click", () => {
+    if(!serviceMenuDisplayed) {
+        servicesCollapsibleMenu.style.display="block";
+        serviceMenuDisplayed = true;
+    } else {
+        servicesCollapsibleMenu.style.display="none";
+        serviceMenuDisplayed = false;
+    }
+})
+
+let applicationsMenuDisplayed = false;
+
+applicationsIcon.addEventListener("click", () => {
+    if(!applicationsMenuDisplayed) {
+        applicationsCollapsibleMenu.style.display="block";
+        applicationsMenuDisplayed = true;
+    } else {
+        applicationsCollapsibleMenu.style.display="none";
+        applicationsMenuDisplayed = false;
+    }
+})
